d49aa62f84bd5fe21291b38434ad95c0543d355d
Derek Tia <dtia05@gmail.com>
2012-04-16 01:05:44 -0700

    add subtraction function

--- instagram_challenge/unscramble.py
+++ instagram_challenge/unscramble.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import urllib
 import os
+import math
 
 def main():
   img_url = 'http://instagram-static.s3.amazonaws.com/images/TokyoPanoramaShredded.png'
@@ -27,15 +28,15 @@ def main():
   # Access an arbitrary pixel. Data is stored as a 2d array where rows are
   # sequential. Each element in the array is a RGBA tuple (red, green, blue,
   # alpha).
-  x, y = 20, 90
+  #x, y = 20, 90
   def get_pixel_value(x, y):
     pixel = data[y * width + x]
     return pixel
-  print get_pixel_value(20, 30)
+  #print get_pixel_value(20, 30)
 
   # Create a new image of the same size as the original
   # and copy a region into the new image
-  NUMBER_OF_COLUMNS = 20
+  NUMBER_OF_COLUMNS = 10
   unshredded = Image.new("RGBA", image.size)
   shred_width = unshredded.size[0]/NUMBER_OF_COLUMNS
   shred_number = 0 
@@ -56,7 +57,25 @@ def main():
     height_inc = height / 10
     height_marker = 0
     for height_marker in range(0, height, height_inc):
-      print get_pixel_value(lx_1, height_marker)
+      #print get_pixel_value(lx_1, height_marker)
+      # compare left of A to right of B
+      diff1 = subtract_rgb(get_pixel_value(lx_1, height_marker), get_pixel_value(rx_2, height_marker))
+      # compare right of A to left of B
+      diff2 = subtract_rgb(get_pixel_value(rx_1, height_marker), get_pixel_value(lx_2, height_marker))
+      # check threshold
+      print 'diff 1: ' + str(diff1)
+      print 'diff 2: ' + str(diff2)
+
+  def subtract_rgb(rgb1, rgb2):
+    dr = rgb1[0] - rgb2[0]
+    dg = rgb1[1] - rgb2[1]
+    db = rgb1[2] - rgb2[2]
+    da = rgb1[3] - rgb2[3]
+    
+    norm_diff = math.sqrt(math.pow(dr, 2) + math.pow(dg, 2) + math.pow(db, 2) + math.pow(da, 2))
+    return (dr, dg, db, da, norm_diff)
+
   compare_strips(strip1, strip2)
+
 if __name__ == '__main__':
   main()
