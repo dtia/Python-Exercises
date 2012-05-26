e0db8939fe39c9e382f9b96b8537e6980b1cf93e
Derek Tia <dtia05@gmail.com>
2012-05-26 08:16:05 -0700

    clean up code

--- evernote_challenge/challenge.py
+++ evernote_challenge/challenge.py
@@ -63,20 +63,5 @@ def sort_sublist(unsorted):
 
   return sorted
 
-"""
-def merge_sort(list):
-  if len(list) is 1:
-    return list
-  elif len(list) is 2:
-    if list[0] > list[1]:
-      return [list[1], list[0]]
-    else:
-      return list
-  else:
-    first_half = len(list)/2
-    second_half = len(list)
-    print 'first half: %s second half: %s' % (first_half, second_half)
-    merge_sort(list[:first_half]).append(merge_sort(list[:second_half]))
-""" 
 if __name__ == '__main__':
   main()
