3d42d7e2e37e8c0e4be2b49559edb2ff669d1e41
Derek Tia <dtia05@gmail.com>
2012-05-26 08:14:43 -0700

    add dropbox

---
+++ dropbox_challenge/count_num.py
@@ -0,0 +1,33 @@
+"""
+1
+11
+21
+1211
+111221
+"""
+
+def main():
+
+def lookandsay(n):
+  current = '1'
+  new_string = ''
+  
+  while(n > 0):
+    while(current not None):
+      for c in current:
+      count_dupes(current)
+    
+    print current
+    n -= 1
+
+def count_dupes(string)
+  count = 0
+  prev = string[0]
+  for c in string:
+    if c == prev:
+      count += 1
+
+    prev = c  
+
+if __name__ == '__main__':
+  main()
