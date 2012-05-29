c1321c250f545aa08b5108f4c7d968c697a5b3d6
Derek Tia <dtia05@gmail.com>
2012-05-28 22:59:29 -0700

    add quicksort

---
+++ evernote_challenge/quicksort.py
@@ -0,0 +1,32 @@
+def main():
+  unordered = [76, 87, 5, -10, 65, 70, 0, 55, 23, -29,  98, 54, 45, 12]
+  print quicksort(unordered)
+
+def quicksort(arr):
+  if len(arr) <= 1:
+    return arr
+
+  pivot = select_pivot(arr)
+  
+  less = []
+  greater = []
+  
+  arr.remove(pivot)
+
+  for x in arr:
+    if x < pivot:
+      less.append(x)
+    else:
+      greater.append(x)
+  
+  temp = quicksort(less)
+  temp.append(pivot)
+  temp.extend(quicksort(greater))
+  return temp
+
+def select_pivot(arr):
+  #pick random index and return its value
+  return arr[len(arr)/2] 
+
+if __name__ == '__main__':
+  main()
