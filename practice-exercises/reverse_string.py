60bfc2ae74814ebb614c5788a73008fadf51db23
Derek Tia <dtia05@gmail.com>
2012-11-15 00:48:00 -0800

    reverse string

--- practice-exercises/reverse_string.py
+++ practice-exercises/reverse_string.py
@@ -1,24 +1,30 @@
 def main():
-  sent = 'Reverse this sentence please.'
-  print 'length of sent: ' + str(len(sent))
+  sent = ' Reverse this sentence please'
   revSent = revString(sent,0,len(sent))
+  
   start = 0
-
   for i in range(len(revSent)):
+    print revSent[i]
     if revSent[i] == ' ':
-      revString(revSent,start,i)
-      start = i
+      print str(start) + ' ' + str(i)
+      revSent = revString(revSent,start,i)
+      start = i+1
 
   print revSent
 
 def revString(word, start, end):
+  print 'the word: ' + word + ' start: ' + str(start) + ' end: ' + str(end)
+  wordlist = list(word)
   length = (end - start) / 2
+  j = 0
   for i in range(start, start+length):
-    temp = word[i]
-    rearIndex = end-i-1
-    print temp
-    word[i] = word[rearIndex]
-    word[rearIndex] = temp
+    temp = wordlist[i]
+    rearIndex = end-j-1
+    wordlist[i] = wordlist[rearIndex]
+    wordlist[rearIndex] = temp
+    j+=1
+
+  return ''.join(wordlist)
 
 if __name__ == '__main__':
   main()
